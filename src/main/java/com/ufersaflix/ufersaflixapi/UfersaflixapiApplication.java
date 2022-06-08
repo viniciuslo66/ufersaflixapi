bc22a4b7ae7cb92925c2cc371b1f8816676bd0da
viniciuslo66 <viniciuslo66@gmail.com>
2022-06-08 15:21:09 -0300

    Funciona pelo amor de Deus

--- src/main/java/com/ufersaflix/ufersaflixapi/UfersaflixapiApplication.java
+++ src/main/java/com/ufersaflix/ufersaflixapi/UfersaflixapiApplication.java
@@ -2,9 +2,10 @@ package com.ufersaflix.ufersaflixapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class UfersaflixapiApplication {
+public class UfersaflixapiApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(UfersaflixapiApplication.class, args);
